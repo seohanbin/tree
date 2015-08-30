7844887a44fb2e6b4739aa2cc78ef03614a81b16
seohanbin <scshb2000@naver.com>
2015-08-30 22:58:22 +0000

    Add length and presence validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,14 @@
 class Todo < ActiveRecord::Base
+    
+    # shb=>length rule
+    validates :title, presence: true
+    #should not be empty
+    validates :title, length: {minimum: 3, maximum: 8 }
+    # min 3character + max 8 char
+    
+    #shb= length rule
+    validates :descript, presence: true
+    validates :descript, length: {minimum: 3 }
+    # min 3 char
+    
 end
